74815e84c17f3250156d7e80d4fdc023c9ef5efa
Huzaifa0888 <115368837+Huzaifa0888@users.noreply.github.com>
2023-05-04 18:04:43 +0500

    done

--- src/Components/tokenomics.jsx
+++ src/Components/tokenomics.jsx
@@ -1,9 +1,9 @@
 
 function Tokenomics() {
   return (
-    <section className=" py-10">
+    <section className=" py-10 overflow-hidden">
       <div className="px-4 mx-auto sm:px-6 lg:px-8  max-w-7xl">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mb-20">
           <span className=" text-center text-[32px] text ">Tokenomics</span>
           <p className="text-white mt-4 text-[18px] font-medium text-center">
             Token name: DGNAPP.AI
@@ -14,8 +14,12 @@ function Tokenomics() {
           <span className="text-white mt-3 text-[18px] flex  font-medium text-center">
             <span className="lg:block hidden"> Token contract: </span>
             0xd068c7c941fbbd2300
-          <span className="lg:block md:block sm:block hidden">  cb2f1841858c2643722dc7</span>
-          <span className="lg:hidden md:hidden sm:hidden block">  ..........</span>
+            <span className="lg:block md:block sm:block hidden">
+              cb2f1841858c2643722dc7
+            </span>
+            <span className="lg:hidden md:hidden sm:hidden block">
+              ..........
+            </span>
           </span>
           <div className="flex justify-center mt-5">
             <a href="#" title="" className="btn text-center" role="button ">
@@ -24,9 +28,12 @@ function Tokenomics() {
           </div>
         </div>
 
-        <div className="lg:flex relative justify-center overflow-x-hidden mt-20 h-[500px] hidden">
-          <div className="w-full h-[500px] overflow-x-hidden absolute">
-            <div className="overflow-x-hidden top-[85px] right-[2%] relative flex items-center justify-end h-[100px] w-[400px]">
+        {/* <div
+          className="lg:flex hidden flex-col items-center 
+ justify-start overflow-x-hidden  relative mt-20 h-[550px] "
+        >
+          <div className="flex justify-center overflow-hidden ">
+            <div className="   flex items-center justify-start  ">
               <div className="text-white">
                 <p className=" text-center text-gray-600"> Supply</p>
                 <p className="text-center font-bold text-[18px]">
@@ -38,26 +45,40 @@ function Tokenomics() {
               </div>
               <div className="ml-[20px] border-r-0 border-b-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
             </div>
-          </div>
-          <div className="w-full h-[500px] overflow-x-hidden absolute">
-            <div className="top-[235px] left-[6%] relative flex items-center justify-end h-[100px] w-[400px]">
+            <div className="">
+              <img src="/tokenomics.png" alt="" className="h-[280px]" />
+            </div>
+            <div className=" flex items-center justify-end  ">
+              <div className="mr-[20px] border-l-0 border-b-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
+
               <div className="text-white">
-                <p className=" text-center text-gray-600"> Taxes</p>
+                <p className=" text-center text-gray-600"> Supply</p>
                 <p className="text-center font-bold text-[18px]">
-                  {" "}
                   0% buy fee 5% sell
                 </p>
                 <p className="text-center font-bold text-[18px]">
-                  {" "}
+                  0% buy fee 5% sell
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:absolute  flex  top-[42%] justify-between  overflow-hidden  ">
+            <div className="  mb-20 flex items-center  justify-end  ">
+              <div className="text-white">
+                <p className=" text-center text-gray-600"> Taxes</p>
+                <p className="text-center font-bold text-[18px]">
+                  0% buy fee 5% sell
+                </p>
+                <p className="text-center font-bold text-[18px]">
                   0% buy fee 5% sell
                 </p>
               </div>
               <div className="ml-[20px] border-r-0 border-b-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
             </div>
-          </div>
-          <div className="w-full h-[500px] overflow-x-hidden absolute ">
-            <div className=" top-[295px] left-[35%] relative   items-center justify-end h-[100px] w-[400px]">
-              <div className=" ml-[200px] top-20 mb-1  border-r-0 border-b-0 border-t-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[30px] opacity-50"></div>
+
+            <div className="   mt-12  w-[400px]">
+              <div className="flex flex-col items-center justify-center    "></div>
+              <div className="ml-[198px] border-r-0 border-b-0 border-t-0 mx-auto w-[100px] border-[1px] border-solid border-[#dc12fc] h-[30px] opacity-50"></div>
 
               <div className=" text-white">
                 <p className=" text-center text-gray-600"> Taxes</p>
@@ -69,43 +90,101 @@ function Tokenomics() {
                 </p>
               </div>
             </div>
-          </div>
-          <div className="w-full h-[500px] overflow-x-hidden absolute">
-            <div className="top-[240px] left-[47.5%] relative flex items-center justify-end h-[100px] w-[400px]">
+            <div className=" flex items-start justify-end  ">
               <div className="mr-[20px] border-l-0 border-b-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
 
               <div className="text-white">
                 <p className=" text-center text-gray-600"> Taxes</p>
                 <p className="text-center font-bold text-[18px]">
-                  {" "}
                   0% buy fee 5% sell
                 </p>
                 <p className="text-center font-bold text-[18px]">
-                  {" "}
                   0% buy fee 5% sell
                 </p>
               </div>
             </div>
           </div>
-          <div className="w-full h-[500px] overflow-x-hidden absolute">
-            <div className="top-[85px] left-[56%] relative flex items-center justify-end h-[100px] w-[400px]">
-              <div className="mr-[20px] border-l-0 border-b-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
+        </div> */}
+       <section>
+
+        <div
+          className="flex lg:hidden flex-col items-center 
+ justify-center overflow-x-hidden  h-full w-full  relative "
+        >
+          <div className="flex justify-center    w-full h-full        ">
+            <div className="flex space-x-52 absolute left-0  right-60 h-full ">
+              <div className="   flex flex-col w-full items-center   ">
+                <div className=" text-white">
+                  <p className=" text-center text-gray-600"> Supply</p>
+                  <p className="text-center font-bold text-[18px]">
+                    1,000,000,000
+                  </p>
+                </div>
+                <div className=" ml-[20px] border-r-0 border-t-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
+              </div>
+              <div className="w-full bottom-3   absolute flex items-center flex-col  h-full ">
+                <div className="text-white w-44  ">
+                  <p className=" text-center text-gray-600"> Taxes</p>
+                  <p className="text-center font-bold text-[18px]">
+                    0% buy fee
+                  </p>
+                  <p className="text-center font-bold text-[18px]">
+                    5% sell fee
+                  </p>
+                  <p className="text-center font-bold text-[18px]">
+                    (2% marketing, 3% burn)
+                  </p>
+                </div>
+                <div className="mr-[20px] border-l-0 border-t-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-[130px] mb-[-160px]  overflow-hidden min-  w-[720px]">
+            <img
+              src="/tokenomics.png"
+              alt=""
+              className="transform -rotate-90 scale-50   w-full h-full
+ "
+            />
+          </div>
+          <div className="  flex justify-center  relative  overflow-hidden  ">
+            <div className="flex items-center justify-center mt-36  relative  w-full flex-col h-full">
+              <div className="ml-[15px] mr-0 border-r-0 border-b-0 mx-auto w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
+
+              <div className="text-white md:w-36 ">
+                <p className=" text-center text-gray-600"> Allocation</p>
+                <p className="text-center font-bold text-[18px]">80% Uniswap</p>
+                <p className="text-center font-bold text-[18px]">
+                  10% Team Wallet
+                </p>
+                <p className="text-center font-bold text-[18px]">
+                  10% Operation Wallet
+                </p>
+              </div>
+            </div>
+            <div className=" relative h-full w-full mt-28 left-12 flex items-center flex-col justify-end  ">
+              <div className="ml-[20px] border-r-0 border-b-0 border-t-0 w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
 
               <div className="text-white">
-                <p className=" text-center text-gray-600"> Supply</p>
                 <p className="text-center font-bold text-[18px]">
-                  0% buy fee 5% sell
-                </p>
-                <p className="text-center font-bold text-[18px]">
-                  0% buy fee 5% sell
+                  Liquidity lock for 1 year
                 </p>
               </div>
             </div>
-          </div>
-          <div className="flex justify-center">
-            <img src="/tokenomics.png" alt="" className="h-[280px]" />
+
+            <div className=" relative  flex items-center flex-col justify-end w-full h-full ">
+              <div className="flex flex-col items-center justify-center    "></div>
+              <div className="ml-[-20px] mr-0 border-l-0 border-b-0 mx-auto w-[20px] border-[1px] border-solid border-[#dc12fc] h-[100px] opacity-50"></div>
+
+              <div className=" text-white">
+                <p className=" text-center text-gray-600"> Taxes</p>
+                <p className="text-center font-bold text-[18px]">Fair</p>
+                <p className="text-center font-bold text-[18px]">Lunch</p>
+              </div>
+            </div>
           </div>
         </div>
+       </section>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-  
+    screens: {
+      xsm: "374px",
+    },
     fontFamily: {
       sans: ['"SansSerifBldFLF"', "serif"],
     },
