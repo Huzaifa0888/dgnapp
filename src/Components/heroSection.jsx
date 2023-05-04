0737fad3c1471fa65757980d8b7868fe469e2584
Huzaifa0888 <115368837+Huzaifa0888@users.noreply.github.com>
2023-05-04 19:57:23 +0500

    done

--- src/Components/heroSection.jsx
+++ src/Components/heroSection.jsx
@@ -6,9 +6,9 @@ function HeroSection() {
       <section className="py-10 bg-[url('/public/background.jpg')] min-h-[200px]  lg:pb-[100px] bg-auto bg-center bg-no-repeat lg:py-10">
         <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
           <div className="grid items-stretch grid-cols-1  gap-x-16 gap-y-12 xl:gap-x-24">
-            <div className="flex items-center justify-start  lg:order-1  ">
+            <div className="flex items-center justify-start lg:h-[700px] lg:order-1  ">
               <div>
-                <h2 className=" text-3xl font-extrabold leading-tight text-white lg:w-[70%] md:w-[85%] sm:text-4xl lg:text-[64px] mt-20  lg:leading-tight">
+                <h2 className=" text-3xl font-extrabold leading-tight text-white lg:w-[70%] md:w-[85%] sm:text-4xl lg:text-[64px]  md:mt-5 lg:leading-tight">
                   Exposure to better data Smarter investing Mitigate risk
                 </h2>
                 <p className="mb-20 text-xl leading-relaxed text-white mt-9 lg:w-[50%]">
